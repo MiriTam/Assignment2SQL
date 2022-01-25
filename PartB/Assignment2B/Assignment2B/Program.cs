ce96abbb59b4dd60739864c5cd350188ca8c378f
Miriam T.G. Aarag <miriam.aarag@no.experis.com>
2022-01-25 13:12:48 +0100

    Added Chinhook database.

---
+++ PartB/Assignment2B/Assignment2B/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assignment2B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
