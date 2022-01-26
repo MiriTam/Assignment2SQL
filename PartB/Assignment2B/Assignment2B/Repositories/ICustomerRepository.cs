4a4701cfe18a1357d531cd612352b29530705cb5
Miriam T.G. Aarag <miriam.aarag@no.experis.com>
2022-01-26 11:24:24 +0100

    Fixed method comments for the ICustomerRepository.

--- PartB/Assignment2B/Assignment2B/Repositories/ICustomerRepository.cs
+++ PartB/Assignment2B/Assignment2B/Repositories/ICustomerRepository.cs
@@ -10,51 +10,56 @@ namespace Assignment2B.Repositories
     internal interface ICustomerRepository
     {
         /// <summary>
-        /// Takes in an int, id of a customer.
+        /// Takes in customer id as an int.
         /// Returns the customer with the given id.
         /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
+        /// <param name="id">Int id of customer.</param>
+        /// <returns>Customer object.</returns>
         public Customer GetCustomer(int id);
+
         /// <summary>
-        /// Takes in a string, name of a customer.
+        /// Takes name of a customer as a string.
         /// Returns the customer with the given name.
         /// </summary>
-        /// <param name="name"></param>
-        /// <returns></returns>
+        /// <param name="name">String name of customer.</param>
+        /// <returns>Customer object.</returns>
         public Customer GetCustomer(string name);
+
         /// <summary>
         /// Returns a  list containing all customers in the database.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>List of customer objects.</returns>
         public List<Customer> GetAllCustomers();
+
         /// <summary>
-        /// Returns a page of customeers that is the specified length long
+        /// Returns a page of given length with customers.
         /// </summary>
-        /// <param name="pageLength"></param>
-        /// <param name="skip"></param>
-        /// <returns></returns>
+        /// <param name="pageLength">Length of customer page.</param>
+        /// <param name="skip">Starting point for customer page.</param>
+        /// <returns>List of customers.</returns>
         public List<Customer> GetPageOfCustomers(int pageLength, int skip);
+
         /// <summary>
-        /// Takes in an int, id of a customer.
-        /// Deletes the customer with the given Id.
+        /// Takes in customer id as an int.
+        /// Deletes the customer with the given id.
         /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
+        /// <param name="id">Int id of customer.</param>
+        /// <returns>Bool indicating if deletion was successfull.</returns>
         public bool DeleteCustomer(int id);
+
         /// <summary>
         /// Takes in a customer.
         /// Adds the new customer to the database.
         /// </summary>
-        /// <param name="customer"></param>
-        /// <returns></returns>
+        /// <param name="customer">String name of customer.</param>
+        /// <returns>Bool indicating if insertion was successfull.</returns>
         public bool AddNewCustomer(Customer customer);
+
         /// <summary>
         /// Updates a customer.
         /// </summary>
-        /// <param name="customer"></param>
-        /// <returns></returns>
+        /// <param name="customer">Customer to be updated.</param>
+        /// <returns>Bool indicating if update was successfull.</returns>
         public bool UpdateCustomer(Customer customer);
-
     }
 }
